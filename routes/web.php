649e54adc2cc489d2dfbb872c8dda09a19e2b8a7
Oecophyllaa <rangga.godzila@gmail.com>
2023-04-27 21:56:35 +0700

    BKPM Minggu 11 | DONE

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,11 @@ Route::group(['namespace' => 'Backend'], function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/upload', 'UploadController@upload')->name('upload');
+Route::post('/upload/proses', 'UploadController@proses_upload')->name('upload.proses');
+Route::post('/upload/resize', 'UploadController@resize_upload')->name('upload.resize');
+Route::get('/dropzone', 'UploadController@dropzone')->name('dropzone');
+Route::post('/dropzone/store', 'UploadController@dropzone_store')->name('dropzone.store');
+Route::get('/pdf_upload', 'UploadController@pdf_upload')->name('pdf.upload');
+Route::post('/pdf/store', 'UploadController@pdf_store')->name('pdf.store');
